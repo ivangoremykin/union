d290edc377a3f34c08def5797fe997832660c7c0
Ivan Goremykin <goremykin.ivan@gmail.com>
2022-12-10 22:50:37 +0000

    Update the links in the Playground

--- Playground/Union.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
+++ Playground/Union.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -2,8 +2,8 @@
 # Union 🦊 🦉 🦌
  
 This playground contains examples for articles:
- - [Adding Union to Swift with Metaprogramming](https://dev.to/ivangoremykin/adding-union-to-swift-with-metaprogramming-510d-temp-slug-2422196?preview=2a0e6b0e13a54f8e81bdf0b3ebda509fea745fbfdab1f59387125d102de27974ffd01ccc7221c38ca39e5719b544d952883627daa299376c05c022cc)
- - [Adding Codable conformance to Union with Metaprogramming](https://dev.to/ivangoremykin/adding-codable-conformance-to-union-with-metaprogramming-4k08-temp-slug-1197351?preview=03668af4c6191b03c0476d24b7040c4149a2f2e50fdcaaad7c75ef0751b0a8613dc87a860e0fdc6cd6a04327a99daf8b68d0619afd0ec73ad73a49b1)
+ - [Adding Union to Swift with Metaprogramming](https://dev.to/ivangoremykin/adding-union-to-swift-with-metaprogramming-510d-temp-slug-2422196)
+ - [Adding Codable conformance to Union with Metaprogramming](https://dev.to/ivangoremykin/adding-codable-conformance-to-union-with-metaprogramming-4k08-temp-slug-1197351)
  
  ## Table of Contents
  - [Generating a Union](Code%20Generation)
